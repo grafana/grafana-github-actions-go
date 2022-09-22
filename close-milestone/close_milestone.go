9bc45b2e668afce73a32da0df2989434d04948cf
yangkb09 <yangkb09@gmail.com>
2022-09-22 14:09:30 -0400

    close-milestone: try to get all milestones

--- close-milestone/close_milestone.go
+++ close-milestone/close_milestone.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	gh "github.com/google/go-github/v47/github"
+)
 
 func main() {
 	fmt.Println("Make it workkkkk")
+
+	milestones, r, e := gh.ListMilestones(_, "yangkb09", "grafana-github-actions-go", _)
+
+	fmt.Println("milestones here!!!", milestones)
+
+	//func (s *IssuesService) ListMilestones(ctx context.Context, owner string, repo string, opts *MilestoneListOptions) ([]*Milestone, *Response, error)
 }
