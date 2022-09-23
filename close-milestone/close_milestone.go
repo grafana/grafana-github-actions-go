6c14a873a99962eb6d0f00d9ef45cac7e1da4d3a
yangkb09 <yangkb09@gmail.com>
2022-09-23 15:21:16 -0400

    close-milestone: try to properly get list of milestones

--- close-milestone/close_milestone.go
+++ close-milestone/close_milestone.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
-	gh "github.com/google/go-github/v47/github"
+	"github.com/google/go-github/v47/github"
 )
 
 func main() {
 	fmt.Println("Make it workkkkk")
-	// Just using something simple to dmeonstrate using the github package here
-	fmt.Println(gh.Bool(true))
+
+	client := github.NewClient(nil)
+	opts := &github.MilestoneListOptions{}
+
+	//func (s *IssuesService) ListMilestones(ctx context.Context, owner string, repo string, opts *MilestoneListOptions) ([]*Milestone, *Response, error)
+	milestones, r, e := client.Issues.ListMilestones(context.Background(), "yangkb09", "grafana-github-actions-go", opts)
+
+	fmt.Println("milestones here!!!", milestones)
+	fmt.Println("response and err!!!", r, e)
 }
