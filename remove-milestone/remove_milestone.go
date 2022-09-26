a8b5622f165b7c4140c6d59d635b5c284188be27
Ying WANG <ying.wang@grafana.com>
2022-09-26 12:54:41 +0200

    add remove milestone github action

---
+++ remove-milestone/remove_milestone.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	gh "github.com/google/go-github/v47/github"
+)
+
+func main() {
+	fmt.Println("Make it workkkkk")
+	// Just using something simple to dmeonstrate using the github package here
+	fmt.Println(gh.Bool(true))
+}
