e885ed05a8eb159a5853994da2502c08b75748fd
yangkb09 <yangkb09@gmail.com>
2022-10-11 16:57:01 -0400

    remove-milestone: add unit tests for readArg, begin unit test for findMilestone

--- remove-milestone/remove_milestone_test.go
+++ remove-milestone/remove_milestone_test.go
@@ -23,15 +23,6 @@ func TestReadArg(t *testing.T) {
 		require.Equal(t, "1234", token)
 		require.Equal(t, "version", currentVersion)
 		require.Nil(t, err)
-		// if token != "1234" {
-		// 	t.Error("token does not equal 1234, token is", token)
-		// }
-		// if currentVersion != "version" {
-		// 	t.Error("current version does not equal version, current version is", currentVersion)
-		// }
-		// if err != nil {
-		// 	t.Error("error is not empty", err.Error())
-		// }
 	})
 }
 
@@ -43,12 +34,6 @@ func TestFindMilestone(t *testing.T) {
 		ms, err := findMilestone(context.Background(), m, "v1.0.0")
 		require.Nil(t, ms)
 		require.ErrorContains(t, err, errorMilestoneNotFound.Error())
-		// if ms != nil {
-		// 	t.Error("milestone should be nil")
-		// }
-		// if !errors.Is(err, errorMilestoneNotFound) {
-		// 	t.Error("error is the wrong type:", err)
-		// }
 	})
 
 	t.Run("If GitHub returns an error, return an error", func(t *testing.T) {
@@ -59,35 +44,6 @@ func TestFindMilestone(t *testing.T) {
 		ms, err := findMilestone(context.Background(), m, "v1.0.0")
 		require.Nil(t, ms)
 		require.ErrorContains(t, err, errorGitHub.Error())
-		// if ms != nil {
-		// 	t.Error("milestone should be nil")
-		// }
-		// if !errors.Is(err, errorGitHub) {
-		// 	t.Error("error is the wrong type:", err)
-		// }
-	})
-}
-
-func TestUpdateMilestone(t *testing.T) {
-	t.Run("Milestone state successfully set to closed", func(t *testing.T) {
-		num := 1
-		ms := gh.Milestone{Number: &num}
-		m := &testMilestoneClient{
-			expectedMilestoneState: "closed",
-		}
-		err := updateMilestone(context.Background(), m, "v1.0.0", &ms)
-		require.NoError(t, err)
-		require.Equal(t, *(ms.State), m.expectedMilestoneState)
-	})
-	t.Run("If GitHub returns an error, return an error", func(t *testing.T) {
-		num := 1
-		ms := gh.Milestone{Number: &num}
-		m := &testMilestoneClient{
-			expectedMilestoneState: "closed",
-			returnError:            true,
-		}
-		err := updateMilestone(context.Background(), m, "v1.0.0", &ms)
-		require.Error(t, err, errors.New("did not find milestone: v1.0.0"))
 	})
 }
 
@@ -111,10 +67,23 @@ func (m *testMilestoneClient) ListMilestones(ctx context.Context, owner string, 
 	return milestones, nil, nil
 }
 
-func (m *testMilestoneClient) EditMilestone(ctx context.Context, owner string, repo string, number int, milestone *gh.Milestone) (*gh.Milestone, *gh.Response, error) {
-	// Check milestone status is definitely closed
+func (m *testMilestoneClient) CreateComment(ctx context.Context, owner string, repo string, number int, comment *gh.IssueComment) (*gh.IssueComment, *gh.Response, error) {
 	if m.returnError {
 		return nil, nil, errors.New("github failed")
 	}
-	return milestone, nil, nil
+	return comment, nil, nil
+}
+
+func (m *testMilestoneClient) ListByRepo(ctx context.Context, owner string, repo string, opts *gh.IssueListByRepoOptions) (issue []*gh.Issue, res *gh.Response, err error) {
+	if m.returnError {
+		return nil, nil, errors.New("github failed")
+	}
+	return issue, nil, nil
+}
+
+func (m *testMilestoneClient) RemoveMilestone(ctx context.Context, owner, repo string, issueNumber int) (issue []*gh.Issue, res *gh.Response, err error) {
+	if m.returnError {
+		return nil, nil, errors.New("github failed")
+	}
+	return issue, nil, nil
 }
