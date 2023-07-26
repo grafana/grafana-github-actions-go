546dce14f71734505709487c8b5b3b6278bfd661
Horst Gutmann <horst.gutmann@grafana.com>
2023-07-26 08:39:25 +0200

    Add communit-release action
    This implements the same functionality as the community post part of the
    update-changelog action. That other implementation is kept in place to
    make migrating less risky.

---
+++ community-release/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/google/go-github/v50/github"
+	"github.com/grafana/grafana-github-actions-go/pkg/changelog"
+	"github.com/grafana/grafana-github-actions-go/pkg/community"
+	"github.com/grafana/grafana-github-actions-go/pkg/toolkit"
+	"github.com/rs/zerolog"
+	"github.com/spf13/pflag"
+)
+
+const inputCommunityAPIKey = "COMMUNITY_API_KEY"
+const inputCommunityAPIUsername = "COMMUNITY_API_USERNAME"
+const inputCommunityCategoryID = "COMMUNITY_CATEGORY_ID"
+const inputCommunityBaseURL = "COMMUNITY_BASE_URL"
+const inputVersion = "VERSION"
+
+func main() {
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	ctx = logger.WithContext(ctx)
+
+	var repo string
+	var doPreview bool
+	var listInputs bool
+
+	pflag.StringVar(&repo, "repo", os.Getenv("GITHUB_REPOSITORY"), "owner/repo pair for a repository on GitHub")
+	pflag.BoolVar(&doPreview, "preview", false, "Only determine the milestone but don't set it")
+	pflag.BoolVar(&listInputs, "list-inputs", false, "Show a list of all available inputs")
+	pflag.Parse()
+
+	logger.Info().Msgf("Operating inside %s", repo)
+
+	tk, err := toolkit.Init(
+		ctx,
+		toolkit.WithRegisteredInput(inputVersion, "Version number to generate the changelog for"),
+		toolkit.WithRegisteredInput(inputCommunityAPIKey, "API token for the Discourse community"),
+		toolkit.WithRegisteredInput(inputCommunityAPIUsername, "API username for the Discourse community"),
+		toolkit.WithRegisteredInput(inputCommunityCategoryID, "Discourse category ID for the changelog post"),
+		toolkit.WithRegisteredInput(inputCommunityBaseURL, "URL where the Discourse community can be found"),
+	)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to initialize toolkit")
+	}
+
+	if listInputs {
+		tk.ShowInputList()
+		return
+	}
+	defer func() {
+		if err := tk.SubmitUsageMetrics(ctx); err != nil {
+			logger.Warn().Err(err).Msg("Failed to submit usage metrics")
+		}
+	}()
+
+	version := tk.MustGetInput(ctx, inputVersion)
+
+	elems := strings.Split(repo, "/")
+	repoOwner := elems[0]
+	repoName := elems[1]
+
+	gh := tk.GitHubClient()
+
+	// We also need the milestone for that release to get the date for the release title:
+	milestone, err := tk.GitHubGQLClient().GetMilestoneByTitle(ctx, repoOwner, repoName, version)
+	if err != nil {
+		logger.Fatal().Err(err).Msgf("No matching milestone found for `%s`", version)
+	}
+	if milestone == nil {
+		logger.Fatal().Msgf("No matching milestone found for `%s`", version)
+	}
+
+	changelogContent, err := retrieveChangelog(ctx, gh, repoOwner, repoName, version)
+	if err != nil {
+		logger.Fatal().Err(err).Msgf("Failed to retrieve changelog for %s", version)
+	}
+
+	releaseTitle := fmt.Sprintf("Changelog: Updates in Grafana %s", version)
+
+	if doPreview {
+		logger.Info().Msgf("No post will be created but this is what it would look like:")
+		fmt.Printf("TITLE: %s\n\n%s\n", releaseTitle, changelogContent)
+		return
+	}
+
+	key := tk.MustGetInput(ctx, inputCommunityAPIKey)
+	username := tk.MustGetInput(ctx, inputCommunityAPIUsername)
+	communityBaseURL := tk.MustGetInput(ctx, inputCommunityBaseURL)
+	rawCommunityCategoryID := tk.MustGetInput(ctx, inputCommunityCategoryID)
+	if key == "" {
+		logger.Fatal().Msgf("No %s provided", tk.GetInputEnvName(inputCommunityAPIKey))
+	}
+	if username == "" {
+		logger.Fatal().Msgf("No %s provided", tk.GetInputEnvName(inputCommunityAPIUsername))
+	}
+	if communityBaseURL == "" {
+		communityBaseURL = "https://community.grafana.com/"
+	}
+	if rawCommunityCategoryID == "" {
+		rawCommunityCategoryID = "9"
+	}
+	communityCategoryID, err := strconv.Atoi(rawCommunityCategoryID)
+	if err != nil {
+		logger.Fatal().Err(err).Msgf("Failed to parse %s", tk.GetInputEnvName(inputCommunityCategoryID))
+	}
+
+	logger.Info().Msg("Posting to the community boards")
+	comm := community.New(
+		community.CommunityWithBaseURL(communityBaseURL),
+		community.CommunityWithAPICredentials(username, key),
+	)
+	if _, err := comm.CreateOrUpdatePost(ctx, community.PostInput{
+		Title:    fmt.Sprintf("Changelog: Updates in Grafana %s", version),
+		Body:     changelogContent,
+		Category: communityCategoryID,
+	}); err != nil {
+		logger.Fatal().Err(err).Msg("Failed to post to the forums")
+	}
+
+}
+
+func retrieveChangelog(ctx context.Context, gh *github.Client, repoOwner string, repoName string, version string) (string, error) {
+	loader := changelog.NewLoader(gh)
+	output, err := loader.LoadContent(ctx, repoOwner, repoName, version, &changelog.LoaderOptions{
+		RemoveHeading: true,
+	})
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf(`%s
+
+[Download page](https://grafana.com/grafana/download/%s)
+[What's new highlights](https://grafana.com/docs/grafana/latest/whatsnew/)`, output, version), nil
+}
