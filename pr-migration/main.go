c16d64491c7ee1287f9329c810b0c3bba4b00580
jev forsberg <jev.forsberg@grafana.com>
2024-11-19 14:33:11 -0700

    baldm0mma/branch_prs/ add logging

--- pr-migration/main.go
+++ pr-migration/main.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/google/go-github/v50/github"
+	"github.com/sethvargo/go-githubactions"
+)
+
+type PullRequestInfo struct{}
+
+func main() {
+	// setup logging
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	// setup github context
+	// get inputs
+	// validate inputs
+	// build github client
+	// get owner and repo from context
+	// get all open pull requests from prevBranch
+	// update base branch for each pull request to nextBranch
+	// notify user of update
+}
